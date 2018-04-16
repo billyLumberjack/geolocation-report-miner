e71abe2c8abce4feeacb9bdacec1d3dbd9726206
Andrea Bergamini <andrea.bergamini@xpeppers.com>
2018-04-16 12:46:31 +0200

    now it can connect to remote DB

--- index.js
+++ index.js
@@ -5,20 +5,22 @@ var password = 'root';
 var shards = ['cluster0-shard-00-00-shxrr.mongodb.net:27017',
     'cluster0-shard-00-01-shxrr.mongodb.net:27017',
     'cluster0-shard-00-02-shxrr.mongodb.net:27017'];
+var database = 'MyDatabase';
 
-var connectionString = 'mongodb://' + username + ':' + password + '@' + shards.join(', ') + '/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+var url = 'mongodb://' + username + ':' + password + '@' + shards.join(',') + '/' + database + '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+console.log(url);
 
-var databaseName = 'MyDatabase';
 var collectionName = 'test-report-collection';
 
-MongoClient.connect(connectionString, function (err, client) {
-        if (null == err) {
-            console.log('Error connecting to server');
-            return;
-        }
-        console.log('Connected successfully to server');
-        var db = client.db(databaseName);
-
-        client.close();
+MongoClient.connect(url, function (err, client) {
+    if (err) {
+        console.log(err);
+        return;
     }
-);
+    var db = client.db('MyDatabase');
+    db.collection(collectionName).findOne({}, function (findErr, result) {
+        if (findErr) throw findErr;
+        console.log(result.name);
+        client.close();
+    });
+});
