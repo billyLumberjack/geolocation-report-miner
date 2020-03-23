77d529f13dc8bdd7531c9c87192dd1c8bdbccb87
smalavasi <samuele.malavasi@amadeus.com>
2020-03-24 00:53:34 +0100

    started but not finished hard core refactoring

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var shards = ['cluster0-shard-00-00-shxrr.mongodb.net:27017',
     'cluster0-shard-00-02-shxrr.mongodb.net:27017'];
 var database = 'MyDatabase';
 
-var url = 'mongodb://' + username + ':' + password + '@' + shards.join(',') + '/' + database + '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+var mongoDbConnectionString = 'mongodb://' + username + ':' + password + '@' + shards.join(',') + '/' + database + '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 
 var collectionName = 'test-report-collection';
 
@@ -31,7 +31,7 @@ var p = {
     skip: 0
 };
 
-var client;
+var globalClient;
 
 var result_to_array_callback = function (err, result){
     if (err) throw err;
@@ -73,56 +73,70 @@ var result_to_array_callback = function (err, result){
     //client.close();
 };
 
-MongoClient.connect(url, function (err, c) {
-    if (err) throw err;
+MongoClient.connect(mongoDbConnectionString, function (error, currentClient) {
 
-    client = c;
+    if (error) throw error;
 
-    c.db('MyDatabase').collection(collectionName).find(p.filter, p.project)
+    currentClient.db('MyDatabase').collection(collectionName).find(p.filter, p.project)
         .sort(p.sort)
         .skip(p.skip)
         .limit(p.limit)
-        .toArray(result_to_array_callback);
+        .toArray(function(error , reportsToLocalizeArray){
+
+            if (error) throw error;
+
+            console.log("Reports to localize : " , JSON.stringify(reportsToLocalizeArray , null ,2));
+
+            var geolocalizedReportArray = reportsToLocalizeArray.map((reportToGeolocalize) => {
+                locate(reportToGeolocalize)
+            });
+            console.log(JSON.stringify(geolocalizedReportArray , null, 2));
+
+        });
+
+    currentClient.close();
         
 });
 
 function locate(report){
-    return new Promise((resolve, reject) => {
-        var opencageURL = "https://api.opencagedata.com/geocode/v1/json?key=efb086e9e0884dc7a05179eb453bf2ab&q=TripName,Region&pretty=0&no_annotations=1";
 
-        opencageURL = opencageURL.replace("TripName", report.SearchTripName);
-        opencageURL = opencageURL.replace("Region", report.Region);
+    var opencageURL = "https://api.opencagedata.com/geocode/v1/json?key=efb086e9e0884dc7a05179eb453bf2ab&q=TripName,Region&pretty=0&no_annotations=1";
 
-        opencageURL = encodeURI(opencageURL); 
+    opencageURL = opencageURL.replace("TripName", report.SearchTripName);
+    opencageURL = opencageURL.replace("Region", report.Region);
 
-        https.get(opencageURL, (response) => {
-            var body = '';
+    opencageURL = encodeURI(opencageURL);
 
-            response.on('data', (chunk) => body += chunk);
+    console.log("Open cage URL " + opencageURL)
 
-            response.on('error', (err) => {
-                throw err;
-            });
+    https.get(opencageURL, (response) => {
+        var body = '';
 
-            response.on('end', () => {
-                //create the object from the html page
-                var location_results = JSON.parse(body);
-                if(location_results.total_results > 0 && location_results.results[0].geometry.lat && location_results.results[0].geometry.lng){
-                    report["geometry"] = {
-                        type: "Point",
-                        coordinates: [
-                            location_results.results[0].geometry.lat,
-                            location_results.results[0].geometry.lng
-                        ]
-                    };
-                    resolve(report);
-                }
-                else{
-                    reject("something wrong with report _id: " + report._id +" while geolocalizing");
-                }
-            });
+        response.on('data', (chunk) => body += chunk);
+
+        response.on('error', (err) => {
+            throw err;
         });
-    });    
+
+        response.on('end', () => {
+            //create the object from the html page
+            var location_results = JSON.parse(body);
+            if(location_results.total_results > 0 && location_results.results[0].geometry.lat && location_results.results[0].geometry.lng){
+                report["geometry"] = {
+                    type: "Point",
+                    coordinates: [
+                        location_results.results[0].geometry.lat,
+                        location_results.results[0].geometry.lng
+                    ]
+                };
+                return report;
+            }
+            else{
+                console.log("something wrong with report _id: " + report._id +" while geolocalizing");
+                return undefined;
+            }
+        });
+    });
 
 
 }
