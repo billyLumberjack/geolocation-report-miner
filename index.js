0924015e7917c6101a954b3a601b17607afb9e3a
smalavasi <samuele.malavasi@amadeus.com>
2020-03-24 22:46:33 +0100

    probably hard core refacroing is finished

--- index.js
+++ index.js
@@ -3,40 +3,13 @@ const https = require('https');
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0 // needed because of winzozz error
 
-var username = 'root';
-var password = 'root';
-var shards = [
-    'cluster0-shard-00-00-shxrr.mongodb.net:27017',
-    'cluster0-shard-00-01-shxrr.mongodb.net:27017',
-    'cluster0-shard-00-02-shxrr.mongodb.net:27017'
-    ];
-var database = 'MyDatabase';
 
-var mongoDbConnectionString = 'mongodb://' + username + ':' + password + '@' + shards.join(',') + '/' + database + '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+function makeConnectionString(username , password , shards , database){
+    return 'mongodb://' + username + ':' + password + '@' + shards.join(',') + '/' + database + '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+}
 
-var collectionName = 'test-report-collection';
-
-var errorCallback = function(findErr, result){
-    if (findErr) throw findErr;
-    console.log(result.name);
-    client.close();
-};
-
-var p = {
-    filter: {
-        geometry : {$exists:false}
-    },
-    project: {},
-    sort: {
-        Date:-1,
-        CreatedAt: -1
-    },
-    limit: 1,
-    skip: 0
-};
-
-var updateInMongoDb = function(localizedReport , mongoDbClient , isLastReport){
-    mongoDbClient.db('MyDatabase').collection(collectionName)
+function updateInMongoDb(localizedReport , mongoDbClient){
+    mongoDbClient.db(database).collection(collectionName)
         .updateOne(
             {"_id" : localizedReport._id},
             {$set : { geometry : localizedReport.geometry }},
@@ -45,20 +18,42 @@ var updateInMongoDb = function(localizedReport , mongoDbClient , isLastReport){
                 console.log(localizedReport._id + " succesfully updated");
             }
         );
-
-    if(isLastReport){
-        mongoDbClient.close();
-    }
 }
+
+var database = 'MyDatabase';
+var collectionName = 'test-report-collection';
+var mongoDbConnectionString = makeConnectionString(
+    'root',
+    'root',
+     [
+        'cluster0-shard-00-00-shxrr.mongodb.net:27017',
+        'cluster0-shard-00-01-shxrr.mongodb.net:27017',
+        'cluster0-shard-00-02-shxrr.mongodb.net:27017'
+    ],
+    database
+);
 
 MongoClient.connect(mongoDbConnectionString, function (error, currentClient) {
 
     if (error) throw error;
 
-    currentClient.db('MyDatabase').collection(collectionName).find(p.filter, p.project)
-        .sort(p.sort)
-        .skip(p.skip)
-        .limit(p.limit)
+    var queryParameters = {
+        filter: {
+            geometry : {$exists:false}
+        },
+        project: {},
+        sort: {
+            Date:-1,
+            CreatedAt: -1
+        },
+        limit: 2000,
+        skip: 0
+    };
+
+    currentClient.db(database).collection(collectionName).find(queryParameters.filter, queryParameters.project)
+        .sort(queryParameters.sort)
+        .skip(queryParameters.skip)
+        .limit(queryParameters.limit)
         .toArray(function(error , reportsToLocalizeArray){
 
             if (error) throw error;
@@ -71,30 +66,23 @@ MongoClient.connect(mongoDbConnectionString, function (error, currentClient) {
 
             console.log("Got " + locatePromises.length + " promises");
 
-            var updatedReports = 0
-
-            var geolocalizedReport = locatePromises.map((locatePromise , currentIndex , promisesArray) => {
-                updatedReports++
-
-                var isLastReport = updatedReports === promisesArray.length
-
-                locatePromise
-                    .then(localizedReport => updateInMongoDb(localizedReport , currentClient , isLastReport))
-                    .catch((error) => {throw error})
-            });
+            Promise.all(locatePromises).then(function(geolocalizedReportArray){
+                geolocalizedReportArray
+                    .filter((gelocalizedReport) => {return gelocalizedReport != undefined})
+                    .forEach(gelocalizedReport => updateInMongoDb(gelocalizedReport , currentClient))
+            })
+            .then(() => {
+                currentClient.close();
+            })
+            .catch((error) => {throw error});
         });
-
-
-
-
 });
 
 function locate(report){
     return new Promise((resolve, reject) => {
-        var opencageURL = "https://api.opencagedata.com/geocode/v1/json?key=efb086e9e0884dc7a05179eb453bf2ab&q=TripName,Region&pretty=0&no_annotations=1";
-
-        opencageURL = opencageURL.replace("TripName", report.SearchTripName);
-        opencageURL = opencageURL.replace("Region", report.Region);
+        var opencageURL = "https://api.opencagedata.com/geocode/v1/json?key=efb086e9e0884dc7a05179eb453bf2ab&q=TripName,Region&pretty=0&no_annotations=1"
+            .replace("TripName", report.SearchTripName)
+            .replace("Region", report.Region);
 
         opencageURL = encodeURI(opencageURL);
 
@@ -104,7 +92,7 @@ function locate(report){
             response.on('data', (chunk) => body += chunk);
 
             response.on('error', (err) => {
-                throw err;
+                reject(err);
             });
 
             response.on('end', () => {
@@ -129,7 +117,7 @@ function locate(report){
                 }
                 else{
                     console.log("something wrong with report _id: " + report._id +" while geolocalizing");
-                    return undefined;
+                    resolve(undefined);
                 }
 
             });
